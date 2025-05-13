bd76ecc3d36c38ab27efe95c8365a0afdc356c85
Suin Kim <contact@suinkim.me>
2025-05-13 20:24:04 +0900

    typed 애니메이션 텍스트 입력 기능 추가

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,37 @@
-const typed = new Typed(".infinite-text", {
-  strings: [
-    "생각나는 대로, 거침없이.",
-    "머리에 떠오르는 걸 바로 써요.",
-    "주저하지 말고, 툭.",
-  ],
-  typeSpeed: 150,
-  backSpeed: 150,
-  fadeOut: false,
-  smartBackspace: true,
-  loop: true,
+const isFocusedTextInput = () => {
+  return $(".text-input").is(":focus");
+};
+
+$(document).ready(() => {
+  // 사용자의 타이핑을 유도하는 타이핑 효과
+  const typed = new Typed(".text-input", {
+    strings: [
+      "생각나는 대로, 거침없이.",
+      "머리에 떠오르는 걸 바로 써요.",
+      "주저하지 말고, 툭.",
+      "생각 많을수록, 그냥 쓰는 게 답.",
+      "생각을 포장하지 말고 그대로.",
+      "지금 떠오른 그거, 바로 써요.",
+      "하고 싶은 말, 그대로 던져요.",
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    attr: "placeholder",
+    bindInputFocusEvents: true,
+    fadeOut: false,
+    loop: true,
+  });
+
+  $(".text-input").on("focus", () => {
+    typed.reset();
+  });
+});
+
+// 한글의 특성상 focus 이후에 조합되는 문자를 유추할 수 없다. 현재로써는
+$(document).on("click keydown", function (e) {
+  const $input = $(".text-input").first();
+
+  if (!$input.is(":focus")) {
+    $input.focus();
+  }
 });
