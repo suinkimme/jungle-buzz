50856cb1df6a31f63fe8721e038b96407c2f3add
Suin Kim <contact@suinkim.me>
2025-05-13 19:34:35 +0900

    typed 애니메이션 텍스트 유형 추가

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,9 @@
 const typed = new Typed(".infinite-text", {
-  strings: ["생각나는 대로, 거침없이."],
+  strings: [
+    "생각나는 대로, 거침없이.",
+    "머리에 떠오르는 걸 바로 써요.",
+    "주저하지 말고, 툭.",
+  ],
   typeSpeed: 150,
   backSpeed: 150,
   fadeOut: false,
